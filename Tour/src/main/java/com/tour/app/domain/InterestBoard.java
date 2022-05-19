2e54de0831a0f4e86e8683252bd04e1494b155e8
Robert0623 <tone8943@gmail.com>
2022-05-19 16:13:36 +0900

    private 접근제어자 붙임

--- Tour/src/main/java/com/tour/app/domain/InterestBoard.java
+++ Tour/src/main/java/com/tour/app/domain/InterestBoard.java
@@ -3,10 +3,10 @@ package com.tour.app.domain;
 import java.util.Date;
 
 public class InterestBoard {
-	int userId; //필수, 사용자 ID
-	int contentsId; //필수, 컨텐츠 ID
-	String interName; //필수X, 컨텐츠 명칭
-	String interTitle; //필수X, 컨텐츠 제목
-	Date regdate; //필수X, 등록일자(자동생성)
+	private int userId; //필수, 사용자 ID
+	private int contentsId; //필수, 컨텐츠 ID
+	private String interName; //필수X, 컨텐츠 명칭
+	private String interTitle; //필수X, 컨텐츠 제목
+	private Date regdate; //필수X, 등록일자(자동생성)
 	
 }
--- Tour/src/main/java/com/tour/app/domain/Member.java
+++ Tour/src/main/java/com/tour/app/domain/Member.java
@@ -3,16 +3,16 @@ package com.tour.app.domain;
 import java.util.Date;
 
 public class Member {
-	int userId; //필수, 사용자 ID
-	String name; //필수, 성명
-	String nickName; //필수, 별명
-	String password; //필수, 패스워드
-	String gender; //필수, 성별
-	String email; //필수, 메일
-	String phone; //필수X, 연락처
-	String interField; //필수, 관심분야
-	String address; //필수X, 주소
-	String role; //필수, 권한역할
-	String withdraw; //필수, 탈퇴여부
-	Date regdate; //필수, 등록일자(자동)
+	private int userId; //필수, 사용자 ID
+	private String name; //필수, 성명
+	private String nickName; //필수, 별명
+	private String password; //필수, 패스워드
+	private String gender; //필수, 성별
+	private String email; //필수, 메일
+	private String phone; //필수X, 연락처
+	private String interField; //필수, 관심분야
+	private String address; //필수X, 주소
+	private String role; //필수, 권한역할
+	private String withdraw; //필수, 탈퇴여부
+	private Date regdate; //필수, 등록일자(자동)
 }

b19ccfebf0a9bf7e465d3b4043f314ce09a3486c
Robert0623 <tone8943@gmail.com>
2022-05-20 15:30:09 +0900

    연관관계맵핑

---
+++ Tour/src/main/java/com/tour/app/domain/InterestBoardPK.java
@@ -0,0 +1,9 @@
+package com.tour.app.domain;
+
+public class InterestBoardPK {
+	//테이블에 지정된 키값의 대상 필드
+	//키 2개를 복합키로 만든다.
+	private int userId;
+	private int contentId;
+	
+}
--- Tour/src/main/java/com/tour/app/domain/TourArea.java
+++ Tour/src/main/java/com/tour/app/domain/TourArea.java
@@ -2,17 +2,38 @@ package com.tour.app.domain;
 
 import java.util.Date;
 
+
+
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.Getter;
+import lombok.Setter;
+
 // 여행 관광지
 //touristarea는 콘텐츠ID   콘텐츠명   구군   테마   장소   제목     이미지URL   썸네일이미지URL 상세정보
+@Getter @Setter
+@Entity
 public class TourArea 
-{
+{		
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int contentsId;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="userid")
+	@Getter @Setter
+	private Member member;
+	
 	private String contentsName;
 	private String area;
 	private String divide; // 테마
@@ -21,7 +42,7 @@ public class TourArea
 	private String imageUrl;
 	private String image2Url;
 	private String detailIntro;
-	private int userId;
+	
 	
 	
 	@CreationTimestamp

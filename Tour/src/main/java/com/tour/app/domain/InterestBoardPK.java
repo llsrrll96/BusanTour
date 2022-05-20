4dfbce7e4f8232ab70b782155dac0a2652acbf13
Robert0623 <tone8943@gmail.com>
2022-05-20 16:02:33 +0900

    Revert "Revert "pk.java 추가""
    This reverts commit feaafc1db92bfe9ea03e57b5a19b5c82a6e3b47c.

--- Tour/src/main/java/com/tour/app/domain/InterestBoardPK.java
+++ Tour/src/main/java/com/tour/app/domain/InterestBoardPK.java
@@ -1,5 +1,11 @@
 package com.tour.app.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
 public class InterestBoardPK {
 	//테이블에 지정된 키값의 대상 필드
 	//키 2개를 복합키로 만든다.

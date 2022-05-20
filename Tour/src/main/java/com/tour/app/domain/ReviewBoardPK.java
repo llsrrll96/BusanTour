257f8c1a685c9b3564d8aba819b4ca34612c0d7d
Admin <Admin@DESKTOP-27NL1TV>
2022-05-20 15:36:12 +0900

    생성자 생성

--- Tour/src/main/java/com/tour/app/domain/ReviewBoardPK.java
+++ Tour/src/main/java/com/tour/app/domain/ReviewBoardPK.java
@@ -2,9 +2,12 @@ package com.tour.app.domain;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor @AllArgsConstructor
 public class ReviewBoardPK implements Serializable
 {
 	private int boardId;

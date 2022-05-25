efaa95b0ef4c34c0d9d80baedc4bfb20f142809d
llsrrll96 <llsrrll96@gmail.com>
2022-05-25 17:06:06 +0900

    tourreviewdto

---
+++ Tour/src/main/java/com/tour/app/dto/TourReviewDTOInterface.java
@@ -0,0 +1,12 @@
+package com.tour.app.dto;
+
+import java.util.Date;
+
+public interface TourReviewDTOInterface 
+{
+	int getNum();
+	String getTitle();
+	String getNick_name();
+	Date getRegdate();
+	int getHitcount();
+}

3645a9c29bae4c78960a62b3924fcf527f6dff59
llsrrll96 <llsrrll96@gmail.com>
2022-08-11 15:20:46 +0900

    add manage Service

--- Tour/src/main/java/com/tour/app/service/ManageService.java
+++ Tour/src/main/java/com/tour/app/service/ManageService.java
@@ -1,9 +1,27 @@
 package com.tour.app.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.tour.app.api.BusanThemaApiExplorer;
+import com.tour.app.domain.RecommenedService;
+import com.tour.app.repository.ManageRepository;
 
 @Service
 public class ManageService 
 {
+	@Autowired
+	private ManageRepository manageRepository;
+	
+	final BusanThemaApiExplorer btae = new BusanThemaApiExplorer();
 
+	public int requestBusanThema(String pageNo, String numOfRows) {
+		List<RecommenedService> busanThemaList= btae.requestAPI(Integer.parseInt(pageNo),Integer.parseInt(numOfRows));
+		return 	manageRepository.saveAll(busanThemaList).size(); // 저장된 갯수
+	}
+
+
+	
 }

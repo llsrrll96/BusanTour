c9b6fa5ddfc4dac8924f1f8110b7877cc9f220e4
llsrrll96 <llsrrll96@gmail.com>
2022-09-17 20:50:05 +0900

    implement Comment

--- Tour/src/main/java/com/tour/app/domain/Comment.java
+++ Tour/src/main/java/com/tour/app/domain/Comment.java
@@ -1,5 +1,48 @@
 package com.tour.app.domain;
 
-public class Comment {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "comments")
+public class Comment 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long commentId;
+	
+	@JoinColumn(name="userId")
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Member member;
+	
+//	@JoinColumn(name= "rboardId")
+//	@ManyToOne(fetch=FetchType.LAZY)
+//	private RoomBoard roomBoard;
+	
+	@Column(length = 1000, nullable = false)
+	private String body;
+	
+	@CreationTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date regdate;
 }
